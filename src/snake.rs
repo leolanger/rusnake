7297576a3bb301b6e69099783efd089316cefcf3
leolanger <leolanger2001@outlook.com>
2020-10-17 18:28:26 +0800

    complete

--- src/snake.rs
+++ src/snake.rs
@@ -111,4 +111,24 @@ impl Snake {
             Direction::Right => (head_x + 1, head_y),
         }
     }
+
+    pub fn restore_tail(&mut self) {
+        let blk = self.tail.clone().unwrap();
+        self.body.push_back(blk);
+    }
+
+    pub fn overlap_tail(&self, x: i32, y: i32) -> bool {
+        let mut ch = 0;
+        for block in &self.body {
+            if x == block.x && y == block.y {
+                return true;
+            }
+
+            ch += 1;
+            if ch == self.body.len() - 1 {
+                break;
+            }
+        }
+        return false;
+    }
 }
