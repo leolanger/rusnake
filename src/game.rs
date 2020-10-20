8b92ca0ee2f306842869f33035de6ca690b924f7
leolanger <leolanger2001@outlook.com>
2020-10-20 23:26:11 +0800

    _

--- src/game.rs
+++ src/game.rs
@@ -13,7 +13,7 @@ const FOOD_COLOR: Color = [0.80, 0.00, 0.00, 1.0];
 const BORDER_COLOR: Color = [0.00, 0.00, 0.00, 1.0];
 const GAMEOVER_COLOR: Color = [0.90, 0.00, 0.00, 0.5];
 
-const MOVING_PERIOD: f64 = 0.1;
+const MOVING_PERIOD: f64 = 0.5;
 const RESTART_TIME: f64 = 1.0;
 
 pub struct Game {
